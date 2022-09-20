12472b45efe63f44ccc93723c94736d244aa442d
Francisco Castaneda <frank.castaneda.g@gmail.com>
2022-09-20 11:06:56 -0500

    cambio de recursos

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@ export const getGifs = async(category) => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=rIMwmHRwOrDeUEuoppBcqxNeEan5Gl7l&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
-    console.log(data);
+    // console.log(data);
     const gifs = data.map( img => ({
         id: img.id,
         title: img.title,
